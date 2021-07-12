b0d0e87c7d1dd41069f06db4da3b4105471b459b
Ashish Kumar <ak238245@gmail.com>
2021-07-12 22:28:49 +0530

    Updates

---
+++ precache-manifest.d23e3023700ec3d04e108d2c04360d89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e8c0d09043c8a6ab4222a407ada14e",
+    "url": "/fotomafia/index.html"
+  },
+  {
+    "revision": "aaaccf1586c9bb98e546",
+    "url": "/fotomafia/static/css/main.296d3dda.chunk.css"
+  },
+  {
+    "revision": "f1a850896effa960d136",
+    "url": "/fotomafia/static/js/2.6b8be091.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/fotomafia/static/js/2.6b8be091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaaccf1586c9bb98e546",
+    "url": "/fotomafia/static/js/main.481857a1.chunk.js"
+  },
+  {
+    "revision": "97cc0180f983941093dc",
+    "url": "/fotomafia/static/js/runtime-main.52b073ba.js"
+  }
+]);
